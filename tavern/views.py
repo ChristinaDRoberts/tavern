5d984604015b08b7849174d55141bb6f968d1832
Christina <Christinepeninsula@hotmail.com>
2019-01-26 15:41:53 -0500

    updated views

--- tavern/views.py
+++ tavern/views.py
@@ -8,6 +8,15 @@ from .models import Location
 class Home(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        available_lunches = Lunch.objects.all()
+
+        context = {
+            "lunch" : available_lunches
+        }
+
+
+        return context
 
 
 class Details(TemplateView):
@@ -22,3 +31,19 @@ class Results(TemplateView):
 
 class Vote(TemplateView):
     template_name = "vote.html"
+
+    #
+    # def get_context_data(self, **kwargs):
+    #     daily_creams = IceCream.objects.filter(available='Daily')
+    #     weekly_creams = IceCream.objects.filter(available='Weekly')
+    #     seasonal_creams = IceCream.objects.filter(available='Seasonal')
+    #     featured_creams = IceCream.objects.filter(featured=True)
+    #
+    #     context = {
+    #         'daily': daily_creams,
+    #         'weekly': weekly_creams,
+    #         'seasonal': seasonal_creams,
+    #         'featured': featured_creams,
+    #     }
+    #
+    #     return context
