fe1e2c8aecbb0165352c9a08baa3595b48977d69
Christina <Christinepeninsula@hotmail.com>
2019-01-26 14:35:00 -0500

    initial set up

---
+++ tavern/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Lunch(models.Model):
+    nickname = models.CharField(max_length = 150)
+    user = models.CharField(max_length = 150)
+    date = models.CharField(max_length = 150)
+
+
+
+    def __str__(self):
+        return self.nickname
+
+
+
+class Location(models.Model):
+    lunch = models.ForeignKey('lunch.Lunch', on_delete=models.CASCADE())
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nickname
---
+++ tavern/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+from .models import
+
+class Home(TemplateView):
+    template_name = 'home.html'
